2878de5eeed3cf3b50ba8a3e86983bc514dddb8b
Thi-Ponce <thi.pponce@gmail.com>
2022-03-10 11:22:56 -0300

    Update likes in html

--- src/modules/displayPokemon.js
+++ src/modules/displayPokemon.js
@@ -4,60 +4,67 @@ export function fetchPokemon() {
 	.then(function(allpokemon) {
 		allpokemon.results.forEach(function(pokemon) {
 			fetchPokemonData(pokemon);
-		})
-	})
+		});
+	});
 }
 
 function fetchPokemonData(pokemon){
 	let url = pokemon.url 
-		fetch(url)
-		.then(response => response.json())
-		.then(function(pokeData){
-		renderPokemon(pokeData);
-		})
-	}
+	fetch(url)
+	.then(response => response.json())
+	.then(function(pokeData){
+	pokeData.likes = 0;
+	renderPokemon(pokeData);
+	});
+}
 
-	function renderPokemon(pokeData){
-		let allPokemonContainer = document.getElementById('cards-container');
-		//create card
-		const pokeContainer = document.createElement('div');
-		pokeContainer.classList.add('card');
-		//insert info
-		const imageContainer = document.createElement('div');
-		imageContainer.classList.add('sprite-container')
-		const pokeName = document.createElement('h4')
-		pokeName.innerText = pokeData.name
-		const pokeNumber = document.createElement('p')
-		pokeNumber.innerText = `#${pokeData.id}`
-		createPokeImage(pokeData.id, imageContainer);
-	
-		//likes 
-		const likesContainer = document.createElement('div');
-		likesContainer.classList.add('likes-container');
-		const likesHeart = document.createElement('i');
-		likesHeart.className = 'far fa-heart';
-		const likesCounter = document.createElement('p');
-		likesCounter.innerHTML = '0';
-		likesContainer.append(likesHeart, likesCounter);
 
-		//comment button
-		const commentButton = document.createElement('button');
-		commentButton.classList.add('comment');
-		commentButton.innerText = 'Comment';
-		commentButton.setAttribute('id', `${pokeData.id}`)
-		commentButton.addEventListener('click', () => {
-			//open the popup
-		});
+function renderPokemon(pokeData) {
+	let allPokemonContainer = document.getElementById('cards-container');
+	//create card
+	const pokeContainer = document.createElement('div');
+	pokeContainer.classList.add('card');
+	//insert info
+	const imageContainer = document.createElement('div');
+	imageContainer.classList.add('sprite-container');
+	const pokeName = document.createElement('h4');
+	pokeName.innerText = pokeData.name;
+	const pokeNumber = document.createElement('p');
+	pokeNumber.innerText = `#${pokeData.id}`;
+	createPokeImage(pokeData.id, imageContainer);
 
-		pokeContainer.append( imageContainer, pokeName, pokeNumber, likesContainer, commentButton);   
+	//likes 
+	const likesContainer = document.createElement('div');
+	likesContainer.classList.add('likes-container');
+	const likesHeart = document.createElement('i');
+	likesHeart.className = 'far fa-heart';
+	likesHeart.setAttribute('id', `${pokeData.id}`);
+	likesHeart.addEventListener('click', () => {
+		pokeData.likes+=1;
+		console.log(pokeData.likes);
+		likesCounter.innerHTML = pokeData.likes;
+		postLike(pokeData.likes, pokeData.name, window.Date);
+	});
+	const likesCounter = document.createElement('p');
+	likesCounter.innerHTML = pokeData.likes;
+	likesContainer.append(likesHeart, likesCounter);
 
-		allPokemonContainer.appendChild(pokeContainer);       
+	//comment button
+	const commentButton = document.createElement('button');
+	commentButton.classList.add('comment');
+	commentButton.innerText = 'Comment';
+	commentButton.setAttribute('id', `${pokeData.id}`);
+	commentButton.addEventListener('click', () => {
+		//open the popup
+	});
+	pokeContainer.append( imageContainer, pokeName, pokeNumber, likesContainer, commentButton);   
 
-		}
+	allPokemonContainer.appendChild(pokeContainer);
+};
 
-		function createPokeImage(pokeID, containerDiv){
-			let pokeImage = document.createElement('img')
-			pokeImage.src =    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png`
-			containerDiv.append(pokeImage);
+	function createPokeImage(pokeID, containerDiv){
+		let pokeImage = document.createElement('img');
+		pokeImage.src =    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png`;
+		containerDiv.append(pokeImage);
 
-		}
+	};
